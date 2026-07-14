8ee02c8210a711a26f380b013c5d818787453a8d
watnee <clintonwatnee@gmail.com>
2026-07-13 21:18:26 -0400

    Initial Commit

---
+++ scripty/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  scripty
+//
+//  Created by Clint Watnee on 7/13/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ scripty/scriptyApp.swift
@@ -0,0 +1,17 @@
+//
+//  scriptyApp.swift
+//  scripty
+//
+//  Created by Clint Watnee on 7/13/26.
+//
+
+import SwiftUI
+
+@main
+struct scriptyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
